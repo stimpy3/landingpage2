6564068063a470bcfc946c73a5761d889604521b
sohan bhadalkar <140563738+stimpy3@users.noreply.github.com>
2024-01-25 05:18:33 +0530

    Update krispr.js

--- krispr.js
+++ krispr.js
@@ -4,12 +4,10 @@ let current = "dark";
 
 modeBtn.addEventListener("click", () => {
   if (current == "dark") {
-    alert("switch to light mode?")
     current = "light";
     html.classList.add("light");
     html.classList.remove("dark");
   } else {
-    alert("switch to dark mode?")
     current = "dark";
     html.classList.remove("light");
     html.classList.remove("dark");
