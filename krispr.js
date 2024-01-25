e725e0b4c2c60b7c27c880543c9e9a361cf19d82
sohan bhadalkar <140563738+stimpy3@users.noreply.github.com>
2024-01-25 09:12:26 +0530

    Update krispr.js
    Added innerHTML (changing text_

--- krispr.js
+++ krispr.js
@@ -1,16 +1,23 @@
 let modeBtn = document.querySelector("#box1btn");
 let html = document.querySelector("body");
 let current = "dark";
+anticurrent="+light mode";
+modeBtn.innerHTML=anticurrent;
 
 modeBtn.addEventListener("click", () => {
   if (current == "dark") {
     current = "light";
+    anticurrent="+dark mode";
+    modeBtn.innerHTML=anticurrent;
     html.classList.add("light");
     html.classList.remove("dark");
   } else {
     current = "dark";
+    anticurrent="+light mode";
+    modeBtn.innerHTML=anticurrent;
     html.classList.remove("light");
     html.classList.remove("dark");
   }
   console.log(current);
 });
+
